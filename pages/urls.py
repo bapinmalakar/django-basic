f0920de56f1cfa5ec45529b9536d3b0845af3280
biplab malakar <biplab.x@venturepact.com>
2018-06-09 18:40:56 +0530

    about template added

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('author_name', views.getAuthorName, name='author_name'),
+    path('about/', views.AboutPageView.as_view(), name="about"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
      template_name = 'home.html';
-
+     
+class AboutPageView(TemplateView):
+    template_name = 'about.html';
 
 # def homePageView(request):
 #     return HttpResponse('Hello, World!');
